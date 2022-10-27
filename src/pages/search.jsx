02005693a27a1a5497f51fd3dbf64451d35df294
Ade Lani <ade@example.com>
2022-10-27 12:30:02 -0700

    side bar added

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 
 export default function Search () {
+    const [nav, setNav] = useState(false)
+
     return (
-        <section className="space-y-4 py-4">
-            <nav className="flex justify-between">
+        <section className="space-y-4 md:space-y-0 pb-4 h-full">
+            <aside 
+                className={`absolute space-y-3 md:hidden text-white top-0 left-0 bottom-0 w-3/4 flex flex-col items-center justify-center h-full bg-[#f5f5f5] transition-all duration-700 ease-in-out -translate-x-full ${nav && "translate-x-[]"}`}
+            >
+                <Link to="/camps/signIn" className="bg-stone-400 w-[90%] p-2 mx-auto inline-block rounded-md hover:bg-black" >LogIn</Link>
+
+                <Link to="/camps/signUp" className="bg-stone-400 w-[90%] p-2 mx-auto inline-block rounded-md hover:bg-black">Sign Up</Link>
+            </aside>
+
+            <nav className="flex justify-between px-2">
                 <div className="flex space-x-3 font-semibold items-center">
                     <Link to="/" ><img src="/Logo.svg" alt="" /></Link>
                     <Link to="/" className="" >Home</Link>
@@ -14,7 +24,8 @@ export default function Search () {
                     <Link to="/camps/signIn" >LogIn</Link>
                     <Link to="/camps/signUp" className="bg-black text-white text-sm p-2 rounded-sm" >Create an account</Link>
                 </div>
-                <button className="md:hidden bg-[#f5f5f5] h-6 flex justify-center items-center px-1 text-xl">
+                <button onClick={()=> setNav(!nav)}
+                        className="md:hidden bg-[#f5f5f5] h-6 flex justify-center items-center px-1 text-xl">
                     <img src="/Hamburger Menu.svg" alt="options" />
                 </button>
             </nav>
